be7eb96bed2fcddc8f719791424bfa08c8ef1496
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-02-01 22:02:51 -0300

    feat(tileset)

--- Codigo/Model/Utils/Espinho.cs
+++ Codigo/Model/Utils/Espinho.cs
@@ -3,9 +3,11 @@ using System;
 
 namespace IgorFoundABug.Codigo.Model.Utils
 {
-    public class Espinho : Sprite
-    {
-
-    }
+	public class Espinho : Sprite
+	{
+		private void _on_StaticBody2D_body_entered(object body)
+		{
+			GD.Print(body);
+		}
+	}
 }
-
