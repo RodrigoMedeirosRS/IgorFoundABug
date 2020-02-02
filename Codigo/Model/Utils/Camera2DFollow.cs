c6c312de375a035fadd8d3b7d5dc237260086892
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-02-01 21:49:11 -0300

    feat(camera e tilesets): - Inserido movimento horizontal da camera. - Inserido tilesets de cenário.

---
+++ Codigo/Model/Utils/Camera2DFollow.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+namespace IgorFoundABug.Codigo.Model.Utils
+{
+    public class Camera2DFollow : Camera2D
+    {
+        public KinematicBody2D player;
+        public override void _Ready()
+        {
+            player = GetParent().GetChild<KinematicBody2D>(0);
+        }
+        public override void _Process(float delta)
+        {
+            Position = new Vector2(player.Position.x, 0);
+        }
+    }
+}
+
---
+++ Codigo/Model/Utils/Espinho.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+namespace IgorFoundABug.Codigo.Model.Utils
+{
+    public class Espinho : Sprite
+    {
+
+    }
+}
+
