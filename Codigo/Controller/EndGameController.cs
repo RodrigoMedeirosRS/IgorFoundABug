bee74f098be19c6068e4ee2c833836edd1461ecb
RMLehnemann <rodrigo.medeiros.email@gmail.com>
2020-02-05 01:32:06 -0300

    feat(ending)

--- Codigo/Controller/EndGameController.cs
+++ Codigo/Controller/EndGameController.cs
@@ -25,52 +25,27 @@ namespace IgorFoundABug.Codigo.Controller
 		}
 		private void Portugues()
 		{
-			texto.Add("Ola Igor");
-			texto.Add("Bem vindo a sua prisão");
-			texto.Add("Nós os programadores");
-			texto.Add("Ficamos com raiva de você");
-			texto.Add("Por causa de todos os bugs");
-			texto.Add("Que você achou nos");
-			texto.Add("nossos códigos");
-			texto.Add("Por isso te prendemos");
-			texto.Add("Em um jogo de Nokia 3310");
-			texto.Add("Impossível de vencer");
-			texto.Add("Aproveite sua prisão");
-			texto.Add("Pois você ficará aqui");
-			texto.Add("para sempre!");
-			texto.Add("Muahahahahahahahaha!");
-			texto.Add("Igor: Não!");
-			texto.Add("Igor: Eu conheço vocês");
-			texto.Add("Igor: Esse jogo deve");
-			texto.Add("Igor: ter bugs!");
-			texto.Add("Igor: Eu vou encontrar");
-			texto.Add("Igor: e usá-los para");
-			texto.Add("Igor: vencer!");
-			texto.Add("Dica: Encontre um bug");
+			texto.Add("Igor: Então!");
+			texto.Add("Igor: Vocês acharam");
+			texto.Add("Igor: que eu queria");
+			texto.Add("Igor: fugir?");
+			texto.Add("Igor: Nãããããão!");
+			texto.Add("Igor: Aqui eu sou");
+			texto.Add("Igor: invencível!");
+            texto.Add("Igor: Chegou a");
+            texto.Add("Igor: Hora de vocês");
 		}
 		private void Ingles()
 		{
-			texto.Add("Hello Igor");
-			texto.Add("Wellcome to your prision");
-			texto.Add("We the programmers");
-			texto.Add("We get mad at you");
-			texto.Add("Because of all the bugs");
-			texto.Add("That you found in");
-			texto.Add("in our codes");
-			texto.Add("That's why we arrested you");
-			texto.Add("In a Nokia 3310 game");
-			texto.Add("Impossible to win");
-			texto.Add("Enjoy your prision");
-			texto.Add("Because you will stay here");
-			texto.Add("forever!");
-			texto.Add("Muahahahahahahahaha!");
-			texto.Add("Igor: No!");
-			texto.Add("Igor: I know you");
-			texto.Add("Igor: This game ");
-			texto.Add("Igor: must have bugs!");
-			texto.Add("Igor: I will find");
-			texto.Add("Igor: and use them to win!");
-			texto.Add("Tip: Find a bug");
+			texto.Add("Igor: So!");
+			texto.Add("Igor: Did you belive");
+			texto.Add("Igor: i want flee?");
+			texto.Add("Igor: Nooooooo!");
+			texto.Add("Igor: Here i am");
+			texto.Add("Igor: Invencible");
+			texto.Add("Igor: Now");
+            texto.Add("Igor: Now it's time for");
+            texto.Add("Igor: for revenge!");
 		}
 		private void _on_Timer_timeout()
 		{
@@ -89,7 +64,7 @@ namespace IgorFoundABug.Codigo.Controller
 		{
 			if (BugsBLL.FlyBug)
 			{
-				Text = "Igor: I found a Bug!";
+				Text = "Igor: Muhahauahua!";
 			}
 		}
     }
